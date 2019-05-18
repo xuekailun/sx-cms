21fce21765acdadd3ca1331a07ecd7bb105fdca9
Moon <511608702@qq.com>
2019-05-18 23:42:56 +0800

    Initial commit

---
+++ src/main/java/com/moon/sxcms/SxCmsApplication.java
@@ -0,0 +1,13 @@
+package com.moon.sxcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SxCmsApplication.class, args);
+    }
+
+}
